3ee2a767c9166af5076488399438eecadd31e5f4
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2022-10-06 19:46:48 -0400

    joyous victory functional and looking great, need to clean up code and add to xp2

--- Honey's Victory Menagerie/Core/Honey_Victory_Menagerie_Colors.sql
+++ Honey's Victory Menagerie/Core/Honey_Victory_Menagerie_Colors.sql
@@ -12,7 +12,7 @@ INSERT INTO Colors
 		(Type, 															Color)
 VALUES	('COLOR_VICTORY_RICHES'						,	'255, 224, 0,	255'),
 		('COLOR_VICTORY_CHARISMA'						,	'149, 241, 220,	255'),
-		('COLOR_VICTORY_JOYOUS'						,	'243, 145, 233,	255');	--can't do rainbow here :(
+		('COLOR_VICTORY_JOYOUS'						,	'255, 0, 128,	255');	--can't do rainbow here :(
 
 
 
--- Honey's Victory Menagerie/Lua/joyous_victory.lua
+++ Honey's Victory Menagerie/Lua/joyous_victory.lua
@@ -13,13 +13,16 @@ local joyousVictoryConcurrentNecessary = GameConfiguration.GetValue("CONFIG_JOYO
 
 function initJoyousVictory()
 
-  local amenityTable = {}
-  for playerid, playerobject in pairs(Players) 
+  print("initing joy victory!")
+
+
+  for playerid, playerobject in pairs(Players) do
 
 	if playerobject:IsMajor() then
 
 		--set to zero if the property doesn't exist yet. If it does exist, assume everything is good.
 		if playerobject:GetProperty("JoyousVictoryTotalTurnsHappiest") == nil then
+		    print("setting a player's total victory values to zero during init!")
 			playerobject:SetProperty("JoyousVictoryTotalTurnsHappiest",0)
 			playerobject:SetProperty("JoyousVictoryTotalTurnsConcurrent",0)
 		end
@@ -35,50 +38,75 @@ function updateJoyousVictoryProgress()
 
   --loop through each player, in each of their cities, and find out how happy everybody is.
   local amenityTable = {}
+  local populationSum = 0
+  local amenitiesSum = 0
+  local population = 0
+  local amenities = 0
   for playerid, playerobject in pairs(Players) do
-
-		if ( playerobject:IsMajor() and playerobject:IsAlive() ) then
-
-		playerCityMembers = playerObject:GetCities()
-		local populationSum = 0
-		local amenitiesSum = 0
-
-		for i, cityObject in playerCityMembers:Members() do
+      populationSum = 0
+      amenitiesSum = 0
     
-			local population = cityObject:GetPopulation()
-			local amenities = tomCity:GetGrowth():GetAmenities()
-			
+	  if ( playerobject:IsMajor() and playerobject:IsAlive() ) then
 
-			populationSum = populationSum + population
-			amenitiesSum = amenitiesSum + amenities
-		end
-		local amenityRatio = 0
-		if (not populationSum == 0 ) then
-			amenityRatio = (amenitiesSum / populationSum)
-		end
-		local playerEntry = { amenityRatio, playerobject }
-		table.insert( amenityTable, playerEntry )
-		end
+		  print("a test")
+
+		  playerCityMembers = playerobject:GetCities()
+		  for i, cityObject in playerCityMembers:Members() do
+			 print("b test")
+    
+			 population = cityObject:GetPopulation()
+			 amenities = cityObject:GetGrowth():GetAmenities()
+
+			 print("amenities")
+			 print(amenities)
+			 print("population")
+			 print(population)
+			
+			 print("population sum in loop a")
+			 print(populationSum)
+			 populationSum = populationSum + population
+			 print("population sum in loop b")
+			 print(populationSum)
+			 amenitiesSum = amenitiesSum + amenities
+		  end
+		  print("population sum in loop c")
+	      print(populationSum)
+		  local amenityRatio = 0
+		  if (not (populationSum == 0) ) then
+			  print("c test")
+			  amenityRatio = (amenitiesSum / populationSum)
+		  end
+		  print("amenityRatio!")
+		  print(amenityRatio)
+		  local playerEntry = { amenityRatio, playerobject }
+		  table.insert( amenityTable, playerEntry )
+	   end
 	end
 
 	--find the best ratio
 	local best_ratio = 0
-	for i, tableEntry in amenityTable do
-		if tableEntry[1] > best_ratio  then
-			best_ratio = tableEntry[1]
+	for index, entry in pairs(amenityTable) do
+		print("amenityvalue!")
+		print(entry[1])
+		if entry[1] > best_ratio  then
+			best_ratio = entry[1]	
 		end
 	end
+
+	print("best value is " .. best_ratio)
 	
 	--increase values for everyone with best ratio, break streak for anyone not there, and check victory for the happy ones
-	for i, tableEntry in amenityTable do
-		if tableEntry[1] == best_ratio  then
-			tableEntry[2]:SetProperty("JoyousVictoryTotalTurnsHappiest", (tableEntry[2]:GetProperty("JoyousVictoryTotalTurnsHappiest") + 1))
-			tableEntry[2]:SetProperty("JoyousVictoryTotalTurnsConcurrent", (tableEntry[2]:GetProperty("JoyousVictoryTotalTurnsConcurrent") + 1))
-			if ( (tableEntry[2]:GetProperty("JoyousVictoryTotalTurnsHappiest") >= joyousVictoryTotalNecessary) and (tableEntry[2]:GetProperty("JoyousVictoryTotalTurnsConcurrent") >= joyousVictoryConcurrentNecessary)
-				grantJoyousVictoryWin(tableEntry[2])
+	for index, entry in pairs(amenityTable) do
+		if entry[1] == best_ratio  then
+			print("incrementing joyous!")
+			entry[2]:SetProperty("JoyousVictoryTotalTurnsHappiest",( entry[2]:GetProperty("JoyousVictoryTotalTurnsHappiest") + 1))
+			entry[2]:SetProperty("JoyousVictoryTotalTurnsConcurrent",(entry[2]:GetProperty("JoyousVictoryTotalTurnsConcurrent") + 1))
+			if ( (entry[2]:GetProperty("JoyousVictoryTotalTurnsHappiest") >= joyousVictoryTotalNecessary) and (entry[2]:GetProperty("JoyousVictoryTotalTurnsConcurrent") >= joyousVictoryConcurrentNecessary) ) then
+				grantJoyousVictoryWin(entry[2])
 			end		
 		else
-			tableEntry[2]:SetProperty("JoyousVictoryTotalTurnsConcurrent", 0)
+			print("decrementing joyous!")
+			entry[2]:SetProperty("JoyousVictoryTotalTurnsConcurrent",0)
 		end
 	end 
 end 
